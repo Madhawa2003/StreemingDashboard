1b44b2e5e1dce61d2a6ce8ce660f38a8a4e7182c
Madhawa mirihagalla <slonline1232@GMAIL.COM>
2025-05-29 05:03:19 +0530

    com 1

--- src/View/components/Hedder.tsx
+++ src/View/components/Hedder.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Logo from '../../Accest/Logo.png'; // adjust the path based on your folder structure
 
 
 export function Hedder() {
+    const [isPulsing, setIsPulsing] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsPulsing(true);        // Start pulse
+            setTimeout(() => setIsPulsing(false), 2000); // Stop after 1s
+        }, 5000); // Every 3 seconds
+
+        return () => clearInterval(interval); // Cleanup
+    }, []);
     return <>
         <div className='h-20  bg-gray-950 '>
             <div className='flex items-center justify-center h-20'>
-                <img src={Logo} alt="Logo" height="10" className='h-24'/>
+                <img src={Logo} alt="Logo" height="10" className={`h-20 w-auto ${isPulsing ? 'animate-pulse' : ''}`}/>
                 <div>
-                    <h1 className='text-teal-400 text-4xl text-center font-serif'> Lets Live</h1>
-                    <h3 className='text-white text-center'>Broadcasting System</h3>
+                    <h1 className='text-teal-400 text-4xl text-center font-serif bg-pulse'> Lets Live</h1>
+                    <h3 className='text-white text-center bg-pulse'>Broadcasting System</h3>
                 </div>
 
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,20 @@ export default {content: [
           '0%, 100%': { backgroundColor: '#6cffa1' },
           '50%': { backgroundColor: '#15803d' },
         },
+        'bg-pulse': {
+          '0%': { height: '6rem' }, // or '96px', etc.
+          '50%': { height: '6.2rem' },
+          '100%': { height: '6rem' }
+
+        },
+
       },
       animation: {
         'bg-flash': 'bg-flash 2s ease-in-out infinite',
+        'bg-pulse': 'bg-pulse 1s ease-in-out infinite',
       },
+
+
       fontFamily: {
         sans: [
           '"Inter"',
